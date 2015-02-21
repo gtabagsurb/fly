bf43a9dd4f14ff7fa7db7521a695033a9f91a702
gtabagsurb <gta@bagsurb.ru>
2015-02-21 12:06:41 +0500

    back

--- assets/3d562eee/jquery.ui.datepicker.js
+++ assets/3d562eee/jquery.ui.datepicker.js
@@ -44,30 +44,19 @@ function Datepicker() {
 		prevText: "Prev", // Display text for previous month link
 		nextText: "Next", // Display text for next month link
 		currentText: "Today", // Display text for current month link
-		monthNames: ['Яварь', 'Февраль', 'Март', 'Апрель',
-			'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
-			'Октябрь', 'Ноябрь', 'Декабрь'], // Names of months for drop-down and formatting
+		monthNames: ["January","February","March","April","May","June",
+			"July","August","September","October","November","December"], // Names of months for drop-down and formatting
 		monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'], // For formatting
+		dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
 		dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
 		dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
 		weekHeader: "Wk", // Column header for week of the year
 		dateFormat: "mm/dd/yy", // See format options on parseDate
-		firstDay: 1, // The first day of the week, Sun = 0, Mon = 1, ...
+		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
 		yearSuffix: "" // Additional text to append to the year in the month headers
 	};
-	/*
-	this.regional['ru'] = {
-		monthNames: ['Яварь', 'Февраль', 'Март', 'Апрель',
-			'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
-			'Октябрь', 'Ноябрь', 'Декабрь'],
-		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-		firstDay: 1,
-	};
-	*/
-	//this.setDefaults(this.regional['ru']);
 	this._defaults = { // Global defaults for all the date picker instances
 		showOn: "focus", // "focus" for popup on focus,
 			// "button" for trigger button, or "both" for either
