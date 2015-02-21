674d0186039a33c92b563bc59e2a36626adba61f
gtabagsurb <gta@bagsurb.ru>
2015-02-21 12:08:19 +0500

    Update and rename js to js/jquery.ui.datepicker-ru.js

---
+++ js/jquery.ui.datepicker-ru.js
@@ -0,0 +1,10 @@
+jQuery(function($){
+  $.datepicker.regional['ru'] = {
+    monthNames: ['Яварь', 'Февраль', 'Март', 'Апрель',
+      'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+      'Октябрь', 'Ноябрь', 'Декабрь'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    firstDay: 1,
+  };
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+});
